9cc10a243edcebd67cb18233c9dc5f8a9899b2de
Ali Anari <ali@anari.me>
2014-01-15 17:59:08 -0800

    added pod
    gitignore
    
    bumped version for updating pod

--- WebService-NationBuilder-0.0100/lib/WebService/NationBuilder.pm
+++ WebService-NationBuilder-0.0100/lib/WebService/NationBuilder.pm
@@ -98,6 +98,23 @@ sub delete_tag {
 
 # ABSTRACT: NationBuilder API bindings
 
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+WebService::NationBuilder - NationBuilder API bindings
+
+=head1 VERSION
+
+version 0.0100
+
 =head1 SYNOPSIS
 
     use WebService::NationBuilder;
@@ -465,6 +482,15 @@ B<Response:>
 
     1
 
-=cut
+=head1 AUTHOR
 
-1;
+Ali Anari <ali@anari.me>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Crowdtilt, Inc..
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
